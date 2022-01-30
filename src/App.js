338ea8c8a110f208308d098afc98e1c895947983
JossBa <jossbattiston@gmail.com>
2022-01-30 17:18:02 +0100

    added coming soon page info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="header"></header>
+      <img src="OHM_Logo.png" width="70%"></img>
+      <h1>coming soon.</h1>
     </div>
   );
 }
